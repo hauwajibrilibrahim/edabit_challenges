53c26785d72c4de54ee1b43e848e2e85b167a6f1
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-10-13 17:57:23 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //Create a function that returns the number of frames shown in a given number of minutes for a certain FPS(frames per second)
-function frames(minutes, fps) {
+  function frames(minutes, fps) {
 	return (minutes * 60) * fps
-}
+  }
