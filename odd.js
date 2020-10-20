8f129e21db9cd6677911d6a3f24c7d62f9323482
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-10-20 11:31:44 +0100

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,2 @@
+//Éowyn has written the function isOdd() to check if a given number is odd or not. Unfortunately, the function does not return the correct result for all the inputs. Help her fix the error
+const isOdd = num => num % 2 != 0;
