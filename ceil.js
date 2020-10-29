76341004ce618d11a70b72e2adc0802d29621e25
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-10-29 18:10:30 +0100

    Add files via upload

---
+++ ceil.js
@@ -0,0 +1,2 @@
+//A typical car can hold four passengers and one driver, allowing five people to travel around. Given n number of people, return how many cars are needed to seat everyone comfortably
+const carsNeeded = n => Math.ceil (n / 5);
