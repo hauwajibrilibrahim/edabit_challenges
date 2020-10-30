3b7df6e2aab14e8f8b92ab747671a55220a1a304
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-10-30 20:23:58 +0100

    Add files via upload

---
+++ volumeofbox.js
@@ -0,0 +1,2 @@
+//Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box
+const volumeOfBox = ({ width, length, height }) => width * length * height;
