e4c61bf5e168433d978a2c870e808406bf5c768f
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-01 23:03:41 +0100

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,8 @@
+//Create a function that determines if the temp of the water is considered boiling or not. temp will be measured in fahrenheit and celsius.
+function isBoiling(temp) {
+	if (temp >= '212F' || temp >= '100C'){
+		return true;
+	} else {
+		return false;
+	}
+}
