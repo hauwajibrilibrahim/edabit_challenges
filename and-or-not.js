da4715ef5b31afd70778c961b03bb47f2172d3a1
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-02 21:11:41 +0100

    Add files via upload

---
+++ and-or-not.js
@@ -0,0 +1,4 @@
+//You will need to write three unfinished logic gates. Continue to write the three logic gates: AND, OR, and NOT
+const NOT = x => +!x
+const AND = (a, b) => a & b
+const OR = (a, b) => a | b;
