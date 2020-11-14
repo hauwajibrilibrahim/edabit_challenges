7613fda07932e1bb2bb0b23f1c45d2fa0aead3af
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-14 21:44:39 +0100

    Add files via upload

---
+++ strNumConv.js
@@ -0,0 +1,2 @@
+//You need to create two functions to substitute toString() and parseInt(); A function called intToString() that converts integers into strings and a function called stringToInt() that converts strings into integers
+const [intToString, stringToInt] = [String, Number];
