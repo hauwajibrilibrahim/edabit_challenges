3edf6643d9227a5348c68e28c93d45573db2ea73
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-14 20:30:42 +0100

    Add files via upload

---
+++ countSyllables.js
@@ -0,0 +1,4 @@
+//Create a function that returns the number of syllables in a simple string. The string is made up of short repeated words like "Lalalalalalala" (which would have 7 syllables).
+function countSyllables(str) {
+	return str.length / 2
+}
