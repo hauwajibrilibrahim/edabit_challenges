51e56a79a0c4f9fdc89bdf0626f59336ec268f43
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-15 23:11:48 +0100

    Add files via upload

---
+++ ValidStrNum.js
@@ -0,0 +1,4 @@
+//Create a function which validates whether a given number exists, and could represent a real life quantity. Inputs will be given as a string
+function validStrNumber(n) {
+	return !isNaN(n)
+}
