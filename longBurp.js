2325086fe398654cd2a17a37b26d312ab02dc388
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-16 20:54:01 +0100

    Add files via upload

---
+++ longBurp.js
@@ -0,0 +1,4 @@
+//Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function
+function longBurp(num) {
+	return "Bu" + "r".repeat(num) + "p";
+}
