bfbc815806a60b1d9e565208754fcd62c1b1e758
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-25 18:34:43 +0100

    Add files via upload

---
+++ discount.js
@@ -0,0 +1,4 @@
+//Create a function that takes two arguments: the final price and the discount percentage as integers and returns the final price after the discount
+function dis(price, discount) {
+    return (100 - discount) / 100 * price;
+}
