4729f9c31b55db0223eaa8a96a85a1a53edb2d2e
hauwajibrilibrahim <58557376+hauwajibrilibrahim@users.noreply.github.com>
2020-11-25 18:55:56 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,15 @@
+//Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers
+function calculator(num1, operator, num2) {
+	switch (operator) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			if (num2 === 0) return "Can't divide by 0!";
+			return num1 / num2;
+	}
+	return null;
+}
